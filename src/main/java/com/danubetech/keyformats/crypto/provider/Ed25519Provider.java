433dd834589d6b34badcd534d00de8d8d41c34bc
Markus Sabadello <markus@danubetech.com>
2023-04-29 09:35:31 +0200

    doc: Add comments about Ed25519 functions.

--- src/main/java/com/danubetech/keyformats/crypto/provider/Ed25519Provider.java
+++ src/main/java/com/danubetech/keyformats/crypto/provider/Ed25519Provider.java
@@ -37,8 +37,39 @@ public abstract class Ed25519Provider {
 		Ed25519Provider.instance = instance;
 	}
 
+	/**
+	 * Generate a new Ed25519 private/public key pair.
+	 * @param publicKey A 32 byte array that will be filled with the new Ed25519 public key.
+	 * @param privateKey A 64 byte array that will be filled with the new Ed25519 private key concatenated with the new Ed25519 public key.
+	 * @throws GeneralSecurityException
+	 */
 	public abstract void generateEC25519KeyPair(byte[] publicKey, byte[] privateKey) throws GeneralSecurityException;
+
+	/**
+	 * Generate a new Ed25519 private/public key pair using a seed.
+	 * @param publicKey A 32 byte array that will be filled with the new Ed25519 public key.
+	 * @param privateKey A 64 byte array that will be filled with the new Ed25519 private key concatenated with the new Ed25519 public key.
+	 * @param seed A seed byte array for generating the new Ed25519 private/public key pair.
+	 * @throws GeneralSecurityException
+	 */
 	public abstract void generateEC25519KeyPairFromSeed(byte[] publicKey, byte[] privateKey, byte[] seed) throws GeneralSecurityException;
+
+	/**
+	 * Sign content using an Ed25519 private key.
+	 * @param content The content to sign.
+	 * @param privateKey A 64 byte array containing a Ed25519 private key concatenated with a Ed25519 public key.
+	 * @return A 64 byte array containing the signature.
+	 * @throws GeneralSecurityException
+	 */
 	public abstract byte[] sign(byte[] content, byte[] privateKey) throws GeneralSecurityException;
+
+	/**
+	 * Verify a signature against given content using an Ed25519 public key.
+	 * @param content The signed content.
+	 * @param signature The signature.
+	 * @param publicKey A 32 byte array containing an Ed25519 public key.
+	 * @return True if the signature can be verified, false otherwise.
+	 * @throws GeneralSecurityException
+	 */
 	public abstract boolean verify(byte[] content, byte[] signature, byte[] publicKey) throws GeneralSecurityException;
 }
