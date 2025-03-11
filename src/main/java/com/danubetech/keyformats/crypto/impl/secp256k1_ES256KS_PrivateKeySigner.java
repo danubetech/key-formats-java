09b71b71b69ae329285a5b66ab2d689414b90d8c
Markus Sabadello <markus@danubetech.com>
2025-03-11 17:12:16 +0100

    refactor: Use different library for Schnorr signatures

--- src/main/java/com/danubetech/keyformats/crypto/impl/secp256k1_ES256KS_PrivateKeySigner.java
+++ src/main/java/com/danubetech/keyformats/crypto/impl/secp256k1_ES256KS_PrivateKeySigner.java
@@ -3,8 +3,12 @@ package com.danubetech.keyformats.crypto.impl;
 import com.danubetech.keyformats.crypto.PrivateKeySigner;
 import com.danubetech.keyformats.crypto.provider.SHA256Provider;
 import com.danubetech.keyformats.jose.JWSAlgorithm;
-import com.miketwk.schnorr.core.Schnorr;
 import org.bitcoinj.crypto.ECKey;
+import org.bitcoinj.secp.api.P256K1KeyPair;
+import org.bitcoinj.secp.api.P256k1PrivKey;
+import org.bitcoinj.secp.api.Secp256k1;
+import org.bitcoinj.secp.bouncy.Bouncy256k1;
+import org.bitcoinj.secp.bouncy.BouncyPrivKey;
 
 import java.security.GeneralSecurityException;
 
@@ -18,10 +22,18 @@ public class secp256k1_ES256KS_PrivateKeySigner extends PrivateKeySigner<ECKey> 
     @Override
     public byte[] sign(byte[] content) throws GeneralSecurityException {
 
-        // sign
+        byte[] signatureBytes;
 
-        byte[] hash = SHA256Provider.get().sha256(content);
-        byte[] signatureBytes = Schnorr.schnorr_sign(hash, this.getPrivateKey().getPrivKey());
+        try (Secp256k1 secp256k1 = new Bouncy256k1()) {
+
+            P256k1PrivKey p256k1PrivKey = new BouncyPrivKey(this.getPrivateKey().getPrivKey());
+            P256K1KeyPair p256K1KeyPair = secp256k1.ecKeyPairCreate(p256k1PrivKey);
+
+            // sign
+
+            byte[] hash = SHA256Provider.get().sha256(content);
+            signatureBytes = secp256k1.schnorrSigSign32(hash, p256K1KeyPair);
+        }
 
         // done
 
--- src/main/java/com/danubetech/keyformats/crypto/impl/secp256k1_ES256KS_PublicKeyVerifier.java
+++ src/main/java/com/danubetech/keyformats/crypto/impl/secp256k1_ES256KS_PublicKeyVerifier.java
@@ -3,8 +3,13 @@ package com.danubetech.keyformats.crypto.impl;
 import com.danubetech.keyformats.crypto.PublicKeyVerifier;
 import com.danubetech.keyformats.crypto.provider.SHA256Provider;
 import com.danubetech.keyformats.jose.JWSAlgorithm;
-import com.miketwk.schnorr.core.Schnorr;
 import org.bitcoinj.crypto.ECKey;
+import org.bitcoinj.secp.api.P256K1XOnlyPubKey;
+import org.bitcoinj.secp.api.P256k1PubKey;
+import org.bitcoinj.secp.api.Result;
+import org.bitcoinj.secp.api.Secp256k1;
+import org.bitcoinj.secp.bouncy.Bouncy256k1;
+import org.bitcoinj.secp.bouncy.BouncyPubKey;
 
 import java.security.GeneralSecurityException;
 
@@ -18,10 +23,20 @@ public class secp256k1_ES256KS_PublicKeyVerifier extends PublicKeyVerifier<ECKey
     @Override
     public boolean verify(byte[] content, byte[] signature) throws GeneralSecurityException {
 
-        // verify
+        boolean verified;
 
-        byte[] hash = SHA256Provider.get().sha256(content);
-        boolean verified = Schnorr.schnorr_verify(hash, this.getPublicKey().getPubKey(), signature);
+        try (Secp256k1 secp256k1 = new Bouncy256k1()) {
+
+            P256k1PubKey p256k1PubKey = new BouncyPubKey(this.getPublicKey().getPubKeyPoint());
+            P256K1XOnlyPubKey p256K1XOnlyPubKey = p256k1PubKey.getXOnly();
+
+            // verify
+
+            byte[] hash = SHA256Provider.get().sha256(content);
+            Result<Boolean> booleanResult = secp256k1.schnorrSigVerify(signature, hash, p256K1XOnlyPubKey);
+            Boolean result = booleanResult == null ? null : booleanResult.get();
+            verified = result == null ? false : result;
+        }
 
         // done
 
